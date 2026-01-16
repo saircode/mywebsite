94bf50662d1eae33712417295399bf2ef4f72059
sairjsanchez <sairjsanchez@gmail.com>
2026-01-16 16:44:05 -0500

    feat: agregar lógica para recargar la página si es la primera carga del sitio

--- app/app.vue
+++ app/app.vue
@@ -6,3 +6,12 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+onMounted(() => {
+  if (!sessionStorage.getItem('saircode_loaded')) {
+    sessionStorage.setItem('saircode_loaded', 'true')
+    window.location.reload()
+  }
+})
+</script>
